311d1bf3412a062462eb9a09bb137effc0c7c81c
Jisie DM <jisie.david@gmail.com>
2019-01-18 13:39:32 -0600

    Walkthrough of Chapter four- Fetching data from json-server API

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,49 +5,38 @@ import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
 
 
-class ApplicationViews extends Component {
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
-
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Angus" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
-
+export default class ApplicationViews extends Component {
     state = {
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI
+        animals: [],
+        employees: []
     }
+
+    componentDidMount() {
+        const newState = {}
+
+        fetch("http://localhost:5002/animals")
+            .then(r => r.json())
+            .then(animals => newState.animals = animals)
+            .then(() => fetch("http://localhost:5002/employees")
+            .then(r => r.json()))
+            .then(employees => newState.employees = employees)
+            .then(() => this.setState(newState))
+    }
+
 
     render() {
         return (
-          <React.Fragment>
+            <React.Fragment>
                 <Route exact path="/" render={(props) => {
                     return <LocationList locations={this.state.locations} />
                 }} />
-                <Route path="/animals" render={(props) => {
+                <Route exact path="/animals" render={(props) => {
                     return <AnimalList animals={this.state.animals} />
                 }} />
-                <Route path="/employees" render={(props) => {
+                <Route exact path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
                 }} />
             </React.Fragment>
         )
     }
 }
-
-export default ApplicationViews
