906bd87b02c6a86052e0f974f7a5714d3e4c0e3c
leahgwin <leahgwin@gmail.com>
2019-01-18 15:23:01 -0600

    Added Chapter 5 Delete Functionality and css for AnimalList

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,42 +1,70 @@
-import { Route } from 'react-router-dom'
-import React, { Component } from "react"
-import AnimalList from './animal/AnimalList'
-import LocationList from './location/LocationList'
-import EmployeeList from './employee/EmployeeList'
-
+import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import AnimalList from "./animal/AnimalList";
+import LocationList from "./location/LocationList";
+import EmployeeList from "./employee/EmployeeList";
 
 export default class ApplicationViews extends Component {
-    state = {
-        animals: [],
-        employees: []
-    }
+  state = {
+    animals: [],
+    employees: []
+  };
 
-    componentDidMount() {
-        const newState = {}
+  deleteAnimal = id => {
+    return fetch(`http://localhost:5002/animals/${id}`, {
+      method: "DELETE"
+    })
+      .then(response => response.json())
+      .then(() => fetch(`http://localhost:5002/animals`))
+      .then(response => response.json())
+      .then(animals =>
+        this.setState({
+          animals: animals
+        })
+      );
+  };
 
-        fetch("http://localhost:5002/animals")
-            .then(r => r.json())
-            .then(animals => newState.animals = animals)
-            .then(() => fetch("http://localhost:5002/employees")
-            .then(r => r.json()))
-            .then(employees => newState.employees = employees)
-            .then(() => this.setState(newState))
-    }
+  componentDidMount() {
+    const newState = {};
 
+    fetch("http://localhost:5002/animals")
+      .then(r => r.json())
+      .then(animals => (newState.animals = animals))
+      .then(() => fetch("http://localhost:5002/employees").then(r => r.json()))
+      .then(employees => (newState.employees = employees))
+      .then(() => this.setState(newState));
+  }
 
-    render() {
-        return (
-            <React.Fragment>
-                <Route exact path="/" render={(props) => {
-                    return <LocationList locations={this.state.locations} />
-                }} />
-                <Route exact path="/animals" render={(props) => {
-                    return <AnimalList animals={this.state.animals} />
-                }} />
-                <Route exact path="/employees" render={(props) => {
-                    return <EmployeeList employees={this.state.employees} />
-                }} />
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Route
+          exact
+          path="/"
+          render={props => {
+            return <LocationList locations={this.state.locations} />;
+          }}
+        />
+        <Route
+          exact
+          path="/animals"
+          render={props => {
+            return (
+              <AnimalList
+                deleteAnimal={this.deleteAnimal}
+                animals={this.state.animals}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/employees"
+          render={props => {
+            return <EmployeeList employees={this.state.employees} />;
+          }}
+        />
+      </React.Fragment>
+    );
+  }
 }
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import dog from "./DogIcon.png";
+import "./Animal.css";
 
-
-class AnimalList extends Component {
+export default class AnimalList extends Component {
   render() {
-        return (
-            <section className="animals">
-            {
-                this.props.animals.map(animal =>
-                    <div key={animal.id}>
-                        {animal.name}
-                    </div>
-                )
-            }
-            </section>
-        )
-    }
+    return (
+      <section className="animals">
+        {this.props.animals.map(animal => (
+          <div key={animal.id} className="card">
+            <div className="card-body">
+              <h5 className="card-title">
+                <img src={dog} className="icon--dog" />
+                {animal.name}
+                <a
+                  href="#"
+                  onClick={() => this.props.deleteAnimal(animal.id)}
+                  className="card-link"
+                >
+                  Delete
+                </a>
+              </h5>
+            </div>
+          </div>
+        ))}
+      </section>
+    );
+  }
 }
-
-export default AnimalList
