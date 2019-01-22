534bbede75f54cada0d8fd51d06be8cf037879b9
leahgwin <leahgwin@gmail.com>
2019-01-22 13:54:28 -0600

    updated locations for practice exercise

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 
-export default class LocationList extends Component {
+class LocationList extends Component {
   render() {
     return (
-      <article>
-        <h3>Student Kennels</h3>
-        <h4>Nashville North Location</h4>
-        <h5>500 Puppy Way</h5>        
-      </article>
+      <section className="locations">
+        <h3>Locations</h3>
+        {this.props.locations.map(location => (
+          <div key={location.id}>
+            <h4>{location.name}</h4>
+            <p>{location.address}</p>
+          </div>
+        ))}
+      </section>
     );
   }
 }
+
+export default LocationList;
