e4c5e19ed8e9cba56a143a3580f0c1dec36cdfb3
Jisie DM <jisie.david@gmail.com>
2019-02-14 11:18:17 -0600

    Email is returned in getProfile

--- src/Auth.js
+++ src/Auth.js
@@ -10,7 +10,7 @@ class Auth {
       clientID: AUTH_CONFIG.clientId,
       redirectUri: 'http://localhost:3000/callback',
       responseType: 'id_token',
-      scope: 'openid profile'
+      scope: 'openid profile email'
     });
 
     this.getProfile = this.getProfile.bind(this);
